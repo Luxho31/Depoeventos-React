94e04c3ac74190994f4cdc77ff27b26481ff9705
Diego Rook <diego.cedron@tryrook.io>
2024-02-29 12:27:04 -0500

    feat: prueba commits

--- src/pages/landing/contact/contact.tsx
+++ src/pages/landing/contact/contact.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
 export default function Contact() {
   return (
-    <div className='bg-green-500 h-screen'>C</div>
-  )
+    <div className="bg-green-500 h-screen">
+      <h1 className="pt-20">Diego el mejor </h1>
+    </div>
+  );
 }
