e159c645ce60d3cf7e39af7b2ac73211d9337220
luxho31 <luisse24@hotmail.com>
2024-03-16 08:27:28 -0500

    feat: Avance Home Dashboard v1

--- src/pages/dashboard/screens/home-dashboard.tsx
+++ src/pages/dashboard/screens/home-dashboard.tsx
@@ -1,8 +1,40 @@
+import { useAuth } from "../../../context/AuthProvider";
+import { Image } from 'antd';
+import banner from "../../../assets/image/AFTER SCHOOL PROGRAM 2024 - DEPOEVENTOS BANNER.png"
+import TypingEffect from "../../../utils/typing-effect";
+import "./home-dashboard.css"
 
 export default function HomeDashboard() {
-  return (
-    <div className="h-screen">
-        <h1>BIENVENIDOS</h1>
-    </div>
-  );
+	const { userInfo } = useAuth();
+
+	return (
+		<>
+			<div className="flex items-center mb-20">
+				<div className="w-[70%] ms-8 font-mono text-5xl">
+					<p className="mb-4">Bienvenido a tu Area Personal</p>
+					<TypingEffect text={userInfo?.firstName + " " + userInfo?.lastName} />
+				</div>
+				<div className=" w-[30%] flex justify-center">
+					<img src="https://static.vecteezy.com/system/resources/previews/026/852/344/original/cute-kid-boy-play-basketball-cartoon-flat-style-illustration-generative-ai-png.png" className="w-1/2 m-8" alt="" />
+				</div>
+			</div>
+			<div className=" flex justify-around">
+				<Image
+					width={500}
+					src={banner}
+					className="rounded-xl shadow-lg"
+				/>
+				<Image
+					width={500}
+					src={banner}
+					className="rounded-xl shadow-lg"
+				/>
+				<Image
+					width={500}
+					src={banner}
+					className="rounded-xl shadow-lg"
+				/>
+			</div>
+		</>
+	);
 }
---
+++ src/utils/typing-effect.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./typing-effect.css"; // Archivo CSS para los estilos de la animación
+
+const TypingEffect = ({ text }: any) => {
+    const [displayText, setDisplayText] = useState("");
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        if (currentIndex === text.length) {
+            return; // Si ya se ha mostrado todo el texto, no hacemos nada más
+        }
+
+        const interval = setInterval(() => {
+            if (currentIndex === text.length) {
+                clearInterval(interval);
+            } else {
+                setDisplayText((prevText) => prevText + text[currentIndex]);
+                setCurrentIndex((prevIndex) => prevIndex + 1);
+            }
+        }, 100); // Tiempo en milisegundos entre cada letra
+
+        return () => clearInterval(interval);
+    }, [text, currentIndex]);
+
+    return (
+        <div className={currentIndex === text.length ? "font-mono font-bold" : "typing-effect"}>
+            {displayText}
+        </div>
+    );
+};
+
+export default TypingEffect;
