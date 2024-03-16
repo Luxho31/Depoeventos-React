2f70460284d6468f9b221277df4ed22a00c5cf24
Diego Rook <diego.cedron@tryrook.io>
2024-03-16 17:08:53 -0500

    feat: avance carrito - falta mostrar productos

--- src/components/modal-product/modal-product.tsx
+++ src/components/modal-product/modal-product.tsx
@@ -6,96 +6,126 @@ import { useAuth } from "../../context/AuthProvider";
 import { useCart } from "../../context/CartProvider";
 
 type Product = {
-    id: number;
-    image: string;
-    title: string;
+  id: number;
+  image: string;
+  title: string;
+  price: number;
+  description: string;
+  product: {
+    name: string;
     price: number;
-    description: string;
-    // Otros campos del producto desde el backend
+    image: string;
+  };
+  children: {
+    name: string;
+  };
+};
+
+type Option = {
+  value: string;
+  label: string;
 };
 
 type ModalProps = {
-    product: Product;
-    onClose: () => void;
+  product: Product;
+  onClose: () => void;
 };
 
 export default function ModalProduct({ product, onClose }: ModalProps) {
-    const { isAuthenticated, cargando } = useAuth();
-    const { addToCart } = useCart();
+  const { isAuthenticated, cargando } = useAuth();
+  const { addToCart } = useCart();
 
-    if (cargando) {
-        return "cargando..."
+  if (cargando) {
+    return "cargando...";
+  }
+
+  const navigate = useNavigate();
+
+  const options: Option[] = [{ value: "hola", label: "hola" }];
+
+  const handleChange = (value: string[]) => {
+    console.log(`selected ${value}`);
+  };
+
+  const handleAddToCart = () => {
+    if (!isAuthenticated) {
+      onClose();
+      navigate("/login");
+    } else {
+      addToCart(product);
+      onClose();
     }
+  };
 
-    const navigate = useNavigate();
-
-    const options: SelectProps['options'] = [
-        { value: "hola", label: "hola" }
-    ];
-
-    const handleChange = (value: string[]) => {
-        console.log(`selected ${value}`);
-    };
-
-    const handleAddToCart = () => {
-        if (!isAuthenticated) {
-            onClose();
-            navigate("/login")
-        } else {
-            addToCart(product);
-            // const storedProducts = JSON.parse(localStorage.getItem('cartProducts') || '[]');
-            // localStorage.setItem('cartProducts', JSON.stringify([...storedProducts, product]));
-            onClose();
-        }
-    };
-
-    return (
-        <motion.div
-            className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900 bg-opacity-50 z-50"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
+  return (
+    <motion.div
+      className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900 bg-opacity-50 z-50"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="bg-white rounded-lg w-[60rem] h-[60%] relative">
+        <button
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
+          onClick={onClose}
         >
-            <div className="bg-white rounded-lg w-[60rem] h-[60%] relative">
-                <button className="absolute top-4 right-4 text-gray-500 hover:text-gray-800" onClick={onClose}>
-                    X
-                </button>
-                <div className="flex h-full">
-                    <div className="w-[40%]">
-                        <img className="w-full h-full object-cover object-center p-4" src={product.image} alt={product.title} />
-                    </div>
-                    <div className="flex flex-col justify-between w-[60%] px-10 py-10">
-                        <div className="flex justify-between items-center h-32">
-                            <h2 className="text-gray-900 font-bold text-3xl">{product.title}</h2>
-                            <p className="text-gray-700 font-bold text-3xl">${product.price}</p>
-                        </div>
-                        <div className="">
-                            <p className="text-gray-600 text-base">{product.description}</p>
-                        </div>
-                        {isAuthenticated ? (
-                            <div className="flex items-center">
-                                <label htmlFor="" className="me-2">Alumnos:</label>
-                                <Select
-                                    mode="multiple"
-                                    allowClear
-                                    style={{ width: '100%' }}
-                                    placeholder="Por favor, selecciona los hijos a matricular"
-                                    onChange={handleChange}
-                                    options={options}
-                                />
-                            </div>
-                        ) : (
-                            <></>
-                        )}
-                        <div className="flex justify-center">
-                            <button className="flex items-center bg-blue-500 px-10 py-3 rounded-lg text-white hover:bg-blue-600" onClick={handleAddToCart}>
-                                {isAuthenticated ? <FaCartPlus className="me-2 text-lg" /> : <FaUserLock className="me-2 text-lg" />}
-                                {isAuthenticated ? "Agregar al carrito" : "Inicia sesión para comprar"}
-                            </button>
-                        </div>
-                    </div>
-                </div>
+          X
+        </button>
+        <div className="flex h-full">
+          <div className="w-[40%]">
+            <img
+              className="w-full h-full object-cover object-center p-4"
+              src={product.image}
+              alt={product.title}
+            />
+          </div>
+          <div className="flex flex-col justify-between w-[60%] px-10 py-10">
+            <div className="flex justify-between items-center h-32">
+              <h2 className="text-gray-900 font-bold text-3xl">
+                {product.title}
+              </h2>
+              <p className="text-gray-700 font-bold text-3xl">
+                ${product.price}
+              </p>
             </div>
-        </motion.div>
-    )
+            <div className="">
+              <p className="text-gray-600 text-base">{product.description}</p>
+            </div>
+            {isAuthenticated ? (
+              <div className="flex items-center">
+                <label htmlFor="" className="me-2">
+                  Alumnos:
+                </label>
+                <Select
+                  mode="multiple"
+                  allowClear
+                  style={{ width: "100%" }}
+                  placeholder="Por favor, selecciona los hijos a matricular"
+                  onChange={handleChange}
+                  options={options}
+                />
+              </div>
+            ) : (
+              <></>
+            )}
+            <div className="flex justify-center">
+              <button
+                className="flex items-center bg-blue-500 px-10 py-3 rounded-lg text-white hover:bg-blue-600"
+                onClick={handleAddToCart}
+              >
+                {isAuthenticated ? (
+                  <FaCartPlus className="me-2 text-lg" />
+                ) : (
+                  <FaUserLock className="me-2 text-lg" />
+                )}
+                {isAuthenticated
+                  ? "Agregar al carrito"
+                  : "Inicia sesión para comprar"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
--- src/pages/landing/products/products.tsx
+++ src/pages/landing/products/products.tsx
@@ -2,98 +2,85 @@ import { useState } from "react";
 import CardProduct from "../../../components/card-product/card-product";
 import ModalProduct from "../../../components/modal-product/modal-product";
 
-export default function Products() {
-
-  type Product = {
-    id: number;
-    image: string;
-    title: string;
+type Product = {
+  id: number;
+  image: string;
+  title: string;
+  price: number;
+  description: string;
+  product: {
+    name: string;
     price: number;
-    description: string;
+    image: string;
   };
+  children: {
+    name: string;
+  };
+};
 
+export default function Products() {
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
-  // Mock data, puedes reemplazarlo con los datos del backend
   const products: Product[] = [
     {
       id: 1,
-      image:
-        "https://www.pngall.com/wp-content/uploads/2016/07/Tom-And-Jerry-PNG.png",
-      title: "Producto 1",
+      image: "https://via.placeholder.com/150",
+      title: "English",
       price: 100,
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas, repellat, possimus atque repellendus accusamus eos quis blanditiis deserunt natus iure similique reiciendis rem mollitia eveniet vitae qui quam autem.",
-      // Otros campos del producto desde el backend
+      description: "Descripción del producto 1",
+      product: {
+        name: "English",
+        price: 100,
+        image: "https://via.placeholder.com/150",
+      },
+      children: {
+        name: "Alumno 1",
+      },
     },
     {
       id: 2,
-      image:
-        "https://i.pinimg.com/originals/4a/bd/c4/4abdc4727c2029fc1ba336742eec954a.png",
+      image: "https://via.placeholder.com/150",
       title: "Producto 2",
-      price: 650,
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas, repellat, possimus atque repellendus accusamus eos quis blanditiis deserunt natus iure similique reiciendis rem mollitia eveniet vitae qui quam autem.",
-      // Otros campos del producto desde el backend
+      price: 200,
+      description: "Descripción del producto 2",
+      product: {
+        name: "Producto 2",
+        price: 200,
+        image: "https://via.placeholder.com/150",
+      },
+      children: {
+        name: "Alumno 2",
+      },
     },
     {
       id: 3,
-      image: "https://fcbk.su/_data/stickers/regular_show/regular_show_00.png",
+      image: "https://via.placeholder.com/150",
       title: "Producto 3",
-      price: 240,
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas, repellat, possimus atque repellendus accusamus eos quis blanditiis deserunt natus iure similique reiciendis rem mollitia eveniet vitae qui quam autem.",
-      // Otros campos del producto desde el backend
+      price: 300,
+      description: "Descripción del producto 3",
+      product: {
+        name: "Producto 3",
+        price: 300,
+        image: "https://via.placeholder.com/150",
+      },
+      children: {
+        name: "Alumno 3",
+      },
     },
     {
       id: 4,
-      image:
-        "https://i.pinimg.com/originals/17/37/21/173721246ff8546baedb7ef457f9d2c1.png",
+      image: "https://via.placeholder.com/150",
       title: "Producto 4",
-      price: 250,
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas, repellat, possimus atque repellendus accusamus eos quis blanditiis deserunt natus iure similique reiciendis rem mollitia eveniet vitae qui quam autem.",
-      // Otros campos del producto desde el backend
-    },
-    {
-      id: 5,
-      image:
-        "https://i.pinimg.com/originals/b6/e3/91/b6e391a89ea14a449782a54317dfde32.png",
-      title: "Producto 5",
-      price: 920,
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas, repellat, possimus atque repellendus accusamus eos quis blanditiis deserunt natus iure similique reiciendis rem mollitia eveniet vitae qui quam autem.",
-      // Otros campos del producto desde el backend
-    },
-    {
-      id: 6,
-      image:
-        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/32a444f6-6389-4360-b323-9e1890d5db5a/dgo1afb-5708707f-0607-4388-b7cd-044a990aa507.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzMyYTQ0NGY2LTYzODktNDM2MC1iMzIzLTllMTg5MGQ1ZGI1YVwvZGdvMWFmYi01NzA4NzA3Zi0wNjA3LTQzODgtYjdjZC0wNDRhOTkwYWE1MDcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.5ZQNGLpGfYjLkNIPZt8JQ8xkjmW6Ph5yyYCvPIIRBcE",
-      title: "Producto 6",
-      price: 330,
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas, repellat, possimus atque repellendus accusamus eos quis blanditiis deserunt natus iure similique reiciendis rem mollitia eveniet vitae qui quam autem.",
-      // Otros campos del producto desde el backend
-    },
-    {
-      id: 7,
-      image:
-        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/78c4ffab-6cb6-45c3-baaa-347c559cb50e/d492bql-1cb4fcbc-6590-4ef9-ac94-8cbc02860ff5.png/v1/fill/w_1600,h_1775/kid_vs__kat_ii_by_achaitanya_d492bql-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTc3NSIsInBhdGgiOiJcL2ZcLzc4YzRmZmFiLTZjYjYtNDVjMy1iYWFhLTM0N2M1NTljYjUwZVwvZDQ5MmJxbC0xY2I0ZmNiYy02NTkwLTRlZjktYWM5NC04Y2JjMDI4NjBmZjUucG5nIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.0LP3ORINS3GblCdxKIdiARcZRLfEKeVnN5RRl0turqw",
-      title: "Producto 7",
-      price: 180,
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas, repellat, possimus atque repellendus accusamus eos quis blanditiis deserunt natus iure similique reiciendis rem mollitia eveniet vitae qui quam autem.",
-      // Otros campos del producto desde el backend
-    },
-    {
-      id: 8,
-      image:
-        "https://img1.picmix.com/output/stamp/normal/7/4/1/8/1668147_ff37a.png",
-      title: "Producto 8",
-      price: 470,
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas, repellat, possimus atque repellendus accusamus eos quis blanditiis deserunt natus iure similique reiciendis rem mollitia eveniet vitae qui quam autem.",
-      // Otros campos del producto desde el backend
+      price: 400,
+      description: "Descripción del producto 4",
+      product: {
+        name: "Producto 4",
+        price: 400,
+        image: "https://via.placeholder.com/150",
+      },
+      children: {
+        name: "Alumno 4",
+      },
     },
   ];
 
