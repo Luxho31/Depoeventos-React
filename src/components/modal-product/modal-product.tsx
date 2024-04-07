6afb7c0176989f02f708d8155333582807972533
Diego Rook <diego.cedron@tryrook.io>
2024-04-07 12:43:42 -0500

    feat: finish modal product responsive

--- src/components/modal-product/modal-product.tsx
+++ src/components/modal-product/modal-product.tsx
@@ -9,6 +9,8 @@ import { useCart } from "../../context/CartProvider";
 import { getChildrensByUserId } from "../../services/children-service";
 
 type Product = {
+  grades: any;
+  ages: any;
   id: number;
   photo: string;
   name: string;
@@ -19,6 +21,7 @@ type Product = {
   maxStudents: number;
   currentStudents: number;
   campus: {
+    map(arg0: (campus: any) => any): unknown;
     id: number;
     name: string;
     description: string;
@@ -117,6 +120,11 @@ export default function ModalProduct({ product, onClose }: ModalProps) {
     }
   };
 
+  const courseNames = product.courses.map((course) => course.name);
+  const campusNames: string[] = (
+    product.campus as unknown as { name: string }[]
+  ).map((campus) => campus.name);
+
   return (
     <motion.div
       className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900 bg-opacity-50 z-50"
@@ -141,74 +149,89 @@ export default function ModalProduct({ product, onClose }: ModalProps) {
           </div>
           <div className="flex flex-col justify-between w-[60%] max-sm:w-full px-10 py-10">
             <div className="flex justify-between items-center p-4 mb-4">
-              <div className="text-gray-900 font-bold text-3xl flex items-center gap-x-2">
-                <Tooltip
-                  title={`Cupos disponibles: ${
-                    product.maxStudents - product.currentStudents
-                  }`}
-                  placement="top"
-                >
-                  <InfoCircleOutlined className="text-base text-gray-500" />
-                </Tooltip>
-                <h2>{product.name}</h2>
+              <div className="text-gray-900 font-bold text-3xl flex items-center gap-x-2 max-sm: ">
+                <div className="flex items-center justify-center max-sm:hidden">
+                  <Tooltip
+                    title={`Cupos disponibles: ${
+                      product.maxStudents - product.currentStudents
+                    }`}
+                    placement="top"
+                  >
+                    <InfoCircleOutlined className="text-base text-gray-500" />
+                  </Tooltip>
+                </div>
+                <h2 className="text-xl">{product.name}</h2>
               </div>
 
-              <p className="text-green-700 font-bold text-2xl">
+              <p className="text-green-700 font-bold text-xl">
                 ${product.price}
               </p>
             </div>
-
-            <div className="mb-4 flex items-center gap-x-2">
-              <FaHome className="text-gray-600 text-lg" />
-              <p className="text-gray-600 text-lg">
-                {product.campus.name} - {product.category.name}
-              </p>
-            </div>
-            <ul className="px-8 py-4">
-              {product.courses.map((course) => (
-                <li key={course.id} className="flex items-center mb-4">
-                  <FaCheck className="text-green-700" />
-                  <p className="ms-4 text-lg font-bold">{course.name}</p>
-                </li>
-              ))}
+            <ul className=" text-gray-400 text-md list-disc max-sm:text-xs ">
+              <li>
+                Se enseñan los siguientes cursos: {courseNames.join(", ")}.
+              </li>
+              <li>
+                {product.courses.length > 1
+                  ? "Los cursos son impartidos en: "
+                  : "El curso es impartido en: "}
+                {campusNames.join(", ")} ({product.category.name}) y comprende
+                los siguientes grados {product.grades.join(", ")} (
+                {product.ages
+                  .sort((a: number, b: number) => a - b)
+                  .map((age: number, index: number, array: number[]) => {
+                    if (index === array.length - 1) {
+                      return age + " años";
+                    } else {
+                      return age + " años, ";
+                    }
+                  })
+                  .join(" ")}
+                ).
+              </li>
             </ul>
-            <div className="mb-4">
-              <p className="text-gray-600 text-lg">
-                Inicio de clases: {product.startDate}
-              </p>
-              <p className="text-gray-600 text-lg">
-                Fin de clases: {product.endDate}
-              </p>
+            <div className="mb-4 flex justify-around gap-y-2 max-sm: mt-3">
+              <div className="flex gap-x-2 text-gray-600 text-sm max-sm:text-xs max-sm:flex-col">
+                <p className="font-semibold">Inicio de clases:</p>
+                <p>{product.startDate}</p>
+              </div>
+
+              <div className="flex gap-x-2 text-gray-600 text-sm max-sm:text-xs max-sm:flex-col">
+                <p className="font-semibold">Fin de clases:</p>
+                <p>{product.endDate}</p>
+              </div>
             </div>
             {isAuthenticated ? (
-              <div className="flex items-center">
-                <label htmlFor="" className="me-2">
+              <div className="flex items-center mb-5">
+                <label htmlFor="" className="me-2 max-sm:text-[0.75rem]">
                   Alumnos:
                 </label>
                 <Select
                   mode="multiple"
                   allowClear
                   className="w-full h-10"
-                  placeholder="Por favor, selecciona los hijos a matricular"
+                  placeholder="Selecciona los hijos a matricular"
                   onChange={handleChange}
                   value={children
                     .filter((child: Children) => child.selected)
-                    .map((child: Children) => child.name)} // Especificar los valores seleccionados
+                    .map((child: Children) => child.name)}
                   options={children.map((child: Children) => ({
                     value: child.name,
                     label: child.name,
                   }))}
+                  // Agregar la clase text-sm solo en dispositivos sm o más pequeños
+                  rootClassName="max-sm:text-xs"
                 />
               </div>
             ) : (
               <></>
             )}
-            <div className="flex justify-center">
+            <div className="flex justify-center ">
               <button
                 className={
                   children.length > 0
-                    ? "flex items-center bg-blue-500 px-10 py-3 rounded-lg text-white hover:bg-blue-600"
-                    : "flex items-center bg-blue-500 px-10 py-3 rounded-lg text-white hover:bg-blue-600"
+                    ? "flex items-center bg-blue-500 px-10 py-3 rounded-lg text-white hover:bg-blue-600 max-sm:text-sm max-sm:px-4 max-sm:py-2"
+                    : "flex items-center bg-blue-500 px-10 py-3 rounded-lg text-white hover:bg-slate-600 max-sm:text-sm max-sm:px-4 max-sm:py-2"
                 }
                 onClick={
                   isAuthenticated
