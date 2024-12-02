6866a67f1008e92087b66acdf29562c6bc0bbc3e
Diego Cedron <70548640+DiegoCedron7@users.noreply.github.com>
2024-12-01 19:41:19 -0500

    Refactor TestimonialDashboard layout and improve testimonial display

--- src/pages/dashboard/screens/opinions-dashboard.tsx
+++ src/pages/dashboard/screens/opinions-dashboard.tsx
@@ -1,39 +1,33 @@
+import { Rating } from "@smastrom/react-rating";
 import { Pagination } from "antd";
 import { useEffect, useState } from "react";
+import { FaCheck } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../context/AuthProvider";
 import {
   getAllTestimonials,
   updateTestimonialApproval,
 } from "../../../services/opinions-service";
-import { FaCheck } from "react-icons/fa";
-import { IoMdClose } from "react-icons/io";
-import { Rating } from "@smastrom/react-rating";
 
 function TestimonialDashboard() {
   const [testimonialData, setTestimonialData] = useState<any[]>([
     {
       id: 1,
-      fullName: "Juan Perez",
-      testimonial: "Excelente servicio",
+      fullName: "Erika Julissa Serrano Rengifo",
+      testimonial:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nunc nec ultricies. kajsldkajsldkjsaldkjasldkjsaldkjsaldkjsadkjaslkdjlsakjdlsakj",
       rating: 5,
       createdAt: "2021-09-01",
       approved: true,
     },
     {
       id: 2,
-      fullName: "Maria Rodriguez",
-      testimonial: "Muy buen servicio",
-      rating: 4,
-      createdAt: "2021-09-02",
-      approved: true,
-    },
-    {
-      id: 3,
-      fullName: "Pedro Gomez",
-      testimonial: "Buen servicio",
-      rating: 3,
-      createdAt: "2021-09-03",
+      fullName: "Diego Alonso Cedrón Serrano",
+      testimonial:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nunc nec ultricies. kajsldkajsldkjsaldkjasldkjsaldkjsaldkjsadkjaslkdjlsakjdlsakj",
+      rating: 1,
+      createdAt: "2021-09-01",
       approved: false,
     },
   ]);
@@ -77,69 +71,75 @@ function TestimonialDashboard() {
   return (
     <div className="h-full">
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Usuario
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Testimonio
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Rating
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Fecha
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Aprobado
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Acciones
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {testimonialData.map((testimonial, index) => (
-              <tr key={index} className="bg-white border-b hover:bg-gray-50">
-                <td className="px-6 py-4">{testimonial.fullName}</td>
-                <td className="px-6 py-4">{testimonial.testimonial}</td>
-                <td className="px-6 py-4">
-                  <Rating
-                    style={{ maxWidth: 80 }}
-                    value={testimonial.rating}
-                    readOnly={true}
-                  />
-                </td>
-                <td className="px-6 py-4">{testimonial.createdAt}</td>
-                <td className="px-6 py-4">
-                  {testimonial.approved ? "Si" : "No"}
-                </td>
-                <td className="px-6 py-4 flex items-center gap-x-3">
-                  <button
-                    className="bg-green-500 text-white rounded-md px-2 py-1"
-                    onClick={() => {
-                      updateTestimonialApproval(testimonial.id);
-                      handleReload();
-                    }}
-                  >
-                    <FaCheck />
-                  </button>
-                  <button
-                    className="bg-red-500 text-white rounded-md px-2 py-1"
-                    onClick={() => {
-                      updateTestimonialApproval(testimonial.id);
-                      handleReload();
-                    }}
-                  >
-                    <IoMdClose />
-                  </button>
-                </td>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr className="w-fit">
+                <th scope="col" className="px-6 py-3 w-fit">
+                  Usuario
+                </th>
+                <th scope="col" className="px-2 py-3 w-fit">
+                  Testimonio
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Rating
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Fecha
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Aprobado
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Acciones
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {testimonialData.map((testimonial, index) => (
+                <tr key={index} className="bg-white border-b hover:bg-gray-50">
+                  <td className="px-6 py-4">{testimonial.fullName}</td>
+                  <td className="px-2 py-4 w-1/2">{testimonial.testimonial}</td>
+                  <td className="px-6 py-4">
+                    <Rating
+                      style={{ maxWidth: 80 }}
+                      value={testimonial.rating}
+                      readOnly={true}
+                    />
+                  </td>
+                  <td className="px-6 py-4">{testimonial.createdAt}</td>
+                  <td
+                    className={`px-6 py-4 font-semibold ${
+                      testimonial.approved ? "text-green-500" : "text-red-500"
+                    }`}
+                  >
+                    {testimonial.approved ? "Si" : "No"}
+                  </td>
+                  <td className="h-full flex mt-6 items-center justify-center gap-x-3">
+                    <button
+                      className="bg-green-500 text-white rounded-md px-2 py-1"
+                      onClick={() => {
+                        updateTestimonialApproval(testimonial.id);
+                        handleReload();
+                      }}
+                    >
+                      <FaCheck />
+                    </button>
+                    <button
+                      className="bg-red-500 text-white rounded-md px-2 py-1"
+                      onClick={() => {
+                        updateTestimonialApproval(testimonial.id);
+                        handleReload();
+                      }}
+                    >
+                      <IoMdClose />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
       <Pagination
         className="mt-4"
